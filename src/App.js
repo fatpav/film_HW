9a325aba292938db3e76f61f0f39e51b2011c700
fatpav <fat_pav@hotmail.com>
2020-12-07 17:02:34 +0000

    some css?

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import FilmConsole from './containers/filmConsole';
 
 function App() {
   return (
-    <>
+    <div classname = "App">
       <h1>
         Go see some films
       </h1>
       <FilmConsole/>
       <a href = 'https://www.imdb.com/calendar/?region=gb'>Check out the latest releases</a>
-    </>
+    </div>
   );
 }
 
